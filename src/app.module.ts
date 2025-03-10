1cec883bd456976481c9ffed81842c5c5b6b688f
lukasfriesenecker <friesenecker.lukas@gmail.com>
2025-03-10 20:56:43 +0100

    .env update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,18 @@ import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 import { BookModule } from './book/book.module';
 import { Book } from './book/book.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Book, User],
       synchronize: true,
     }),
